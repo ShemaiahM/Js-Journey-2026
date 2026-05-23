c3658401f22f355ada32177141064ec0ebceb24f
Shemaiah M <shemkmmillard@gmail.com>
2026-05-23 19:21:10 +0100

    Add files via upload
    simple comments

---
+++ JsBasic.js
@@ -0,0 +1,13 @@
+//comments in Js 
+// single line
+console.log("Yellow World")
+
+console.log( /*this is a comment but ignored, yet still logs hello world in console*/ "Hello Wolrd");
+/*
+console.log("can you hear me");
+console.log("hello");
+console.log("hello...");
+console.log("oh wait...");
+*/
+console.log("The rest of the 'console.log'/s' are basically written out ");
+console.log("as you can see the rest of the words are written out");
